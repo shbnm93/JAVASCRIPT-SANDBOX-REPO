4cf8eb045819524bccf37ac81dce0e13fe30880b
shbnm93 <shabnamhamidi1993@gmail.com>
2023-07-26 18:56:22 +0300

    folders created

---
+++ 01-variable-data-types/01-console/script.js
@@ -0,0 +1,25 @@
+console.log(100);
+
+
+console.log('Hello World');
+
+console.log(20, 'Hello', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({name:'shabnam', email:'shabnamhamidi1993@gmail.com'});
+
+console.group('simple');
+
+
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: black; color:green';
+
+console.log('%c Hello world', styles);
